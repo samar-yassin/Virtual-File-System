7875a48374ff90cb718ef4bc3ac1e3e1486905f1
Abd-Elhadi <abdelhadioumar@gmail.com>
2021-05-31 22:28:35 +0200

    updated Indexed class

--- Indexed.java
+++ Indexed.java
@@ -1,16 +1,35 @@
 public class Indexed implements AllocationTechniques {
-    @Override
-    public void allocate(FreeSpaceManger manger, File1 file1) {
 
+    @Override
+    public void allocate(FreeSpaceManger manger, File1 f) {
+        int indexBlock = -1;
+        boolean flag = false;
+        //++requiredBlocks;
+        f.setSize(f.getSize() + 1);
+        for (int i = 0; i < f.getSize(); i++) {
+            if (f.blocks.get(i).isAvailable()) {
+                if (!flag) { // index block not yet allocated
+                    f.blocks.get(i).setAvailable(false);
+                    indexBlock = i;
+                    flag = true;
+                } else {    // index block allocated
+                    f.blocks.get(i).setAvailable(false);
+                    f.blocks.get(indexBlock).getData().add(i);
+                }
+                //if (f.blocks.get(indexBlock).getData().size() == manger.getDiskSize())
+                    //return indexBlock;
+            }
+        }
+        //if (indexBlock != -1)
+            //de_allocate(indexBlock);
+        //return -1;
     }
 
     @Override
-    public void deallocate(FreeSpaceManger manger, File1 file1) {
-
-    }
-
-    @Override
-    public String toString() {
-        return "Indexed";
+    public void deallocate(FreeSpaceManger manger, File1 f) {
+        for (int i : f.blocks.get(f.blockStart).getData())
+            f.blocks.get(i).setAvailable(true);
+        f.blocks.get(f.blockStart).setAvailable(true);
+        f.blocks.get(f.blockStart).getData().clear();
     }
 }
